c918564add8055395c7e31bde0f2941c8f181f7d
Agustín De Giorgi <dgagustin01@gmail.com>
2023-05-14 15:16:48 -0300

    FIX: Change controller request mapping name

--- src/main/java/com/example/demo/controller/DragonBallController.java
+++ src/main/java/com/example/demo/controller/DragonBallController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.github.javafaker.Faker;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/dragon-ball/characters")
+@RequestMapping("/api/v1/dragon-ball-failover/characters")
 public class DragonBallController {
 
     private final Faker faker = new Faker();
-    private List<String> characters = new ArrayList<>();
+    private final List<String> characters = new ArrayList<>();
+    private static final Logger LOG = LoggerFactory.getLogger(DragonBallController.class);
+
 
     @PostConstruct
     public void init() {
@@ -26,6 +30,7 @@ public class DragonBallController {
 
     @GetMapping
     public ResponseEntity<List<String>> get() {
+        LOG.info("Getting Dragon Ball FAILOVER characters");
         return ResponseEntity.ok(characters);
     }
 }
